6a4db4ebd4fb614e07a2fe9d0365eaefc1430e33
vidrepar <vid.repar@gmail.com>
2018-04-19 11:20:08 +0200

    Nunjucks configuration; Error handlers;

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# marketcloud-starter
+# A barebones Marketcloud starter based on Express
+
+## Change log
+ - Added and configured nunjucks
+ - Development error handler which emits stack traces
+ - Product error handler
+ - 404 handler middleware
+
+## To Do
+  - Add linter
+  - Add Marketcloud initialization code block ;)
+  - Write a barebones logic of how to use Marketcloud on index route
+  - Move change log elsewhere
+
+### Reach out
+For any feature requests send me an email or create an issue.
--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var stylus = require('stylus');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const stylus = require('stylus');
+const nunjucks = require('nunjucks');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.set('view engine', 'nunjucks');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,5 +27,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Development error handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
